83278ccbb93bf69cfa8139973af9d8a115798f83
Alexander Vitchinov <vitalex92@yandex.ru>
2021-10-22 10:08:40 +0300

    basic arch

---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./MonthStatistics.module.scss";
+
+interface Props {
+  
+}
+
+export const MonthStatistics = (props: Props) => {
+  return (
+    <div>
+      Month Statistics
+    </div>
+  )
+}
